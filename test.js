dd2bbd98ca54982c4a2ff41cbde0704a0508b05b
Dokome <1098265675@qq.com>
2024-06-23 14:37:07 +0800

    feat: mr 测试

---
+++ test.js
@@ -0,0 +1,199 @@
+const a = {
+  ref: "refs/heads/main",
+  before: "ad5897637cc6977c940cf8685ae35e10e5d682bc",
+  after: "6d01bc057ddece252f679bafc6e3b1761a44d909",
+  repository: {
+    id: 818614392,
+    node_id: "R_kgDOMMsQeA",
+    name: "ai-codereview",
+    full_name: "Dokome/ai-codereview",
+    private: false,
+    owner: {
+      name: "Dokome",
+      email: "51528647+Dokome@users.noreply.github.com",
+      login: "Dokome",
+      id: 51528647,
+      node_id: "MDQ6VXNlcjUxNTI4NjQ3",
+      avatar_url: "https://avatars.githubusercontent.com/u/51528647?v=4",
+      gravatar_id: "",
+      url: "https://api.github.com/users/Dokome",
+      html_url: "https://github.com/Dokome",
+      followers_url: "https://api.github.com/users/Dokome/followers",
+      following_url:
+        "https://api.github.com/users/Dokome/following{/other_user}",
+      gists_url: "https://api.github.com/users/Dokome/gists{/gist_id}",
+      starred_url: "https://api.github.com/users/Dokome/starred{/owner}{/repo}",
+      subscriptions_url: "https://api.github.com/users/Dokome/subscriptions",
+      organizations_url: "https://api.github.com/users/Dokome/orgs",
+      repos_url: "https://api.github.com/users/Dokome/repos",
+      events_url: "https://api.github.com/users/Dokome/events{/privacy}",
+      received_events_url:
+        "https://api.github.com/users/Dokome/received_events",
+      type: "User",
+      site_admin: false,
+    },
+    html_url: "https://github.com/Dokome/ai-codereview",
+    description: null,
+    fork: false,
+    url: "https://github.com/Dokome/ai-codereview",
+    forks_url: "https://api.github.com/repos/Dokome/ai-codereview/forks",
+    keys_url: "https://api.github.com/repos/Dokome/ai-codereview/keys{/key_id}",
+    collaborators_url:
+      "https://api.github.com/repos/Dokome/ai-codereview/collaborators{/collaborator}",
+    teams_url: "https://api.github.com/repos/Dokome/ai-codereview/teams",
+    hooks_url: "https://api.github.com/repos/Dokome/ai-codereview/hooks",
+    issue_events_url:
+      "https://api.github.com/repos/Dokome/ai-codereview/issues/events{/number}",
+    events_url: "https://api.github.com/repos/Dokome/ai-codereview/events",
+    assignees_url:
+      "https://api.github.com/repos/Dokome/ai-codereview/assignees{/user}",
+    branches_url:
+      "https://api.github.com/repos/Dokome/ai-codereview/branches{/branch}",
+    tags_url: "https://api.github.com/repos/Dokome/ai-codereview/tags",
+    blobs_url:
+      "https://api.github.com/repos/Dokome/ai-codereview/git/blobs{/sha}",
+    git_tags_url:
+      "https://api.github.com/repos/Dokome/ai-codereview/git/tags{/sha}",
+    git_refs_url:
+      "https://api.github.com/repos/Dokome/ai-codereview/git/refs{/sha}",
+    trees_url:
+      "https://api.github.com/repos/Dokome/ai-codereview/git/trees{/sha}",
+    statuses_url:
+      "https://api.github.com/repos/Dokome/ai-codereview/statuses/{sha}",
+    languages_url:
+      "https://api.github.com/repos/Dokome/ai-codereview/languages",
+    stargazers_url:
+      "https://api.github.com/repos/Dokome/ai-codereview/stargazers",
+    contributors_url:
+      "https://api.github.com/repos/Dokome/ai-codereview/contributors",
+    subscribers_url:
+      "https://api.github.com/repos/Dokome/ai-codereview/subscribers",
+    subscription_url:
+      "https://api.github.com/repos/Dokome/ai-codereview/subscription",
+    commits_url:
+      "https://api.github.com/repos/Dokome/ai-codereview/commits{/sha}",
+    git_commits_url:
+      "https://api.github.com/repos/Dokome/ai-codereview/git/commits{/sha}",
+    comments_url:
+      "https://api.github.com/repos/Dokome/ai-codereview/comments{/number}",
+    issue_comment_url:
+      "https://api.github.com/repos/Dokome/ai-codereview/issues/comments{/number}",
+    contents_url:
+      "https://api.github.com/repos/Dokome/ai-codereview/contents/{+path}",
+    compare_url:
+      "https://api.github.com/repos/Dokome/ai-codereview/compare/{base}...{head}",
+    merges_url: "https://api.github.com/repos/Dokome/ai-codereview/merges",
+    archive_url:
+      "https://api.github.com/repos/Dokome/ai-codereview/{archive_format}{/ref}",
+    downloads_url:
+      "https://api.github.com/repos/Dokome/ai-codereview/downloads",
+    issues_url:
+      "https://api.github.com/repos/Dokome/ai-codereview/issues{/number}",
+    pulls_url:
+      "https://api.github.com/repos/Dokome/ai-codereview/pulls{/number}",
+    milestones_url:
+      "https://api.github.com/repos/Dokome/ai-codereview/milestones{/number}",
+    notifications_url:
+      "https://api.github.com/repos/Dokome/ai-codereview/notifications{?since,all,participating}",
+    labels_url:
+      "https://api.github.com/repos/Dokome/ai-codereview/labels{/name}",
+    releases_url:
+      "https://api.github.com/repos/Dokome/ai-codereview/releases{/id}",
+    deployments_url:
+      "https://api.github.com/repos/Dokome/ai-codereview/deployments",
+    created_at: 1719052885,
+    updated_at: "2024-06-23T06:26:21Z",
+    pushed_at: 1719124163,
+    git_url: "git://github.com/Dokome/ai-codereview.git",
+    ssh_url: "git@github.com:Dokome/ai-codereview.git",
+    clone_url: "https://github.com/Dokome/ai-codereview.git",
+    svn_url: "https://github.com/Dokome/ai-codereview",
+    homepage: null,
+    size: 1,
+    stargazers_count: 0,
+    watchers_count: 0,
+    language: null,
+    has_issues: true,
+    has_projects: true,
+    has_downloads: true,
+    has_wiki: true,
+    has_pages: false,
+    has_discussions: false,
+    forks_count: 0,
+    mirror_url: null,
+    archived: false,
+    disabled: false,
+    open_issues_count: 0,
+    license: null,
+    allow_forking: true,
+    is_template: false,
+    web_commit_signoff_required: false,
+    topics: [],
+    visibility: "public",
+    forks: 0,
+    open_issues: 0,
+    watchers: 0,
+    default_branch: "main",
+    stargazers: 0,
+    master_branch: "main",
+  },
+  pusher: { name: "Dokome", email: "51528647+Dokome@users.noreply.github.com" },
+  sender: {
+    login: "Dokome",
+    id: 51528647,
+    node_id: "MDQ6VXNlcjUxNTI4NjQ3",
+    avatar_url: "https://avatars.githubusercontent.com/u/51528647?v=4",
+    gravatar_id: "",
+    url: "https://api.github.com/users/Dokome",
+    html_url: "https://github.com/Dokome",
+    followers_url: "https://api.github.com/users/Dokome/followers",
+    following_url: "https://api.github.com/users/Dokome/following{/other_user}",
+    gists_url: "https://api.github.com/users/Dokome/gists{/gist_id}",
+    starred_url: "https://api.github.com/users/Dokome/starred{/owner}{/repo}",
+    subscriptions_url: "https://api.github.com/users/Dokome/subscriptions",
+    organizations_url: "https://api.github.com/users/Dokome/orgs",
+    repos_url: "https://api.github.com/users/Dokome/repos",
+    events_url: "https://api.github.com/users/Dokome/events{/privacy}",
+    received_events_url: "https://api.github.com/users/Dokome/received_events",
+    type: "User",
+    site_admin: false,
+  },
+  created: false,
+  deleted: false,
+  forced: false,
+  base_ref: null,
+  compare:
+    "https://github.com/Dokome/ai-codereview/compare/ad5897637cc6...6d01bc057dde",
+  commits: [
+    {
+      id: "6d01bc057ddece252f679bafc6e3b1761a44d909",
+      tree_id: "11017737b256c570127efab3b8928d28443c1db9",
+      distinct: true,
+      message: "feat: heh31e",
+      timestamp: "2024-06-23T14:29:18+08:00",
+      url: "https://github.com/Dokome/ai-codereview/commit/6d01bc057ddece252f679bafc6e3b1761a44d909",
+      author: [Object],
+      committer: [Object],
+      added: [],
+      removed: [],
+      modified: [Array],
+    },
+  ],
+  head_commit: {
+    id: "6d01bc057ddece252f679bafc6e3b1761a44d909",
+    tree_id: "11017737b256c570127efab3b8928d28443c1db9",
+    distinct: true,
+    message: "feat: heh31e",
+    timestamp: "2024-06-23T14:29:18+08:00",
+    url: "https://github.com/Dokome/ai-codereview/commit/6d01bc057ddece252f679bafc6e3b1761a44d909",
+    author: { name: "Dokome", email: "1098265675@qq.com", username: "Dokome" },
+    committer: {
+      name: "Dokome",
+      email: "1098265675@qq.com",
+      username: "Dokome",
+    },
+    added: [],
+    removed: [],
+    modified: ["README.md"],
+  },
+};
